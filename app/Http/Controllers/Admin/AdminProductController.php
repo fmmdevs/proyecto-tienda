939fd6009b643ebdea6207748b145a146e66a6ba
fmmdevs <fmm.devs@gmail.com>
2024-04-17 13:55:04 +0200

    Added create product in admin section

--- app/Http/Controllers/Admin/AdminProductController.php
+++ app/Http/Controllers/Admin/AdminProductController.php
@@ -15,4 +15,24 @@ class AdminProductController extends Controller
         $viewData['products'] = Product::all();
         return view('admin.product.index')->with('viewData', $viewData);
     }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            "name" => "required|max:255",
+            "description" => "required",
+            "price" => "required|decimal:0,2|gt:0",
+            "image" => "image",
+        ]);
+
+        $newProduct = new Product();
+        $newProduct->setName($request->input('name'));
+        $newProduct->setDescription(($request->input('description')));
+        $newProduct->setPrice($request->input('price'));
+        $newProduct->setImage("europe-trees.jpeg");
+
+        $newProduct->save();
+
+        return back();
+    }
 }
--- database/migrations/2024_03_29_103643_create_products_table.php
+++ database/migrations/2024_03_29_103643_create_products_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->string('name');
             $table->text('description');
             $table->string('image');
-            $table->decimal('price', 6, 2, true);
+            $table->decimal('price', 10, 2, true);
             $table->timestamps();
         });
     }
