8e7fef879fbda091a913d6f9ceff90262f8807c7
fmmdevs <fmm.devs@gmail.com>
2024-04-19 11:24:11 +0200

    Added login and registrer

--- app/Http/Controllers/Admin/AdminProductController.php
+++ app/Http/Controllers/Admin/AdminProductController.php
@@ -19,12 +19,8 @@ class AdminProductController extends Controller
 
     public function store(Request $request)
     {
-        $request->validate([
-            "name" => "required|max:255",
-            "description" => "required",
-            "price" => "required|decimal:0,2|gt:0",
-            "image" => "image",
-        ]);
+        // Validations in model
+        Product::validate($request);
 
         $newProduct = new Product();
         $newProduct->setName($request->input('name'));
@@ -62,12 +58,8 @@ class AdminProductController extends Controller
 
     public function update(Request $request, $id)
     {
-        $request->validate([
-            "name" => "required|max:255",
-            "description" => "required",
-            "price" => "required|numeric|gt:0",
-            "image" => "image",
-        ]);
+        // Validations in model
+        Product::validate($request);
 
         $product = Product::findOrFail($id);
         $product->setName($request->input('name'));
--- routes/web.php
+++ routes/web.php
@@ -52,3 +52,7 @@ Route::get('/admin/products/{id}/edit', 'App\Http\Controllers\Admin\AdminProduct
 
 Route::put('/admin/products/{id}/update', '\App\Http\Controllers\Admin\AdminProductController@update')
     ->name("admin.product.update");
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
